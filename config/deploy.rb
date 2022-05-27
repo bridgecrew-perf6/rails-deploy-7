6eda06f4d8c34e6d7b71497b97ed32703ee8bdce
fumtas1k <fumta01a7@gmail.com>
2022-05-27 17:08:29 +0900

    fix:構文修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :keep_releases, 5
 
 set :rbenv_type, :user
 set :rbenv_ruby, File.read(".ruby-version").strip
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exce"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
 set :puma_threads, [0, 5]
 set :puma_workers, 2
